8d8c3f54a0901700f72f9ff74104fa7aef4f3620
fabioStori <fabstx@gmail.com>
2022-07-16 00:13:47 -0300

    div-test

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ max_time = 60*60
 
 files = os.listdir('InstancesCSP')
 
-# for file in files:
-#   tabu_search = TS_CSP(ternure_porcent=0.2, iterations=10000, max_time=max_time, instance_file=file, improve='first', const_heuristic='greedy')
-#   best_solution, total_time = tabu_search.solve(seed=2)
-#   f = open('all_instances_ts_random_fi.out', 'a')
-#   f.write(f"{file}  {best_solution.cost}  {total_time} \n")
-#   f.close()
+for file in files:
+  tabu_search = TS_CSP(ternure_porcent=0.2, iterations=1000, max_time=max_time, instance_file=file, improve='best', const_heuristic='greedy', probabilistic_ts=False, max_iter_no_improve=100, diversification=True, diversificate_in=1)
+  best_solution, total_time = tabu_search.solve(seed=2)
+  f = open('all_instances_ts_diversification_bi.out', 'a')
+  f.write(f"{file}  {best_solution.cost}  {total_time} \n")
+  f.close()
 
 # tabu_search = TS_CSP(ternure_porcent=0.3, iterations=1000, max_time=max_time, instance_file='berlin52-9.csp2', improve='best', const_heuristic='greedy')
-tabu_search = TS_CSP(
-  ternure_porcent=0.2,
-  iterations=1000,
-  max_time=60*60,
-  instance_file='eil51-11.csp2',
-  improve='best',
-  const_heuristic='greedy',
-  probabilistic_ts=True,
-  # max_iter_no_improve=500,
-  diversification=True,
-  diversificate_in=10
-)
-best_solution, total_time = tabu_search.solve(seed=2)
+# tabu_search = TS_CSP(
+#   ternure_porcent=0.2,
+#   iterations=1000,
+#   max_time=60*60,
+#   instance_file='kroA200-7.csp2',
+#   improve='best',
+#   const_heuristic='greedy',
+#   probabilistic_ts=False,
+#   max_iter_no_improve=100,
+#   diversification=True,
+#   diversificate_in=1
+# )
+# best_solution, total_time = tabu_search.solve(seed=2)
 
-print(best_solution, total_time)
+# print(best_solution, total_time)
